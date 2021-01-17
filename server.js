574f07f6f116d8b0740aa2ed29282325f1923691
Mohamad Handy Nugraha <handynugraha29@gmail.com>
2021-01-17 15:06:27 +0700

    add express body-parser, color, connect mongoDB, create unhandleRejection

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan')
-
-// Route files
-const bootcamps = require('./routes/bootcamps')
+const connectDB = require('./config/db')
+const colors = require('colors');
 
 // load env variables
 dotenv.config({
   path: './config/config.env'
 })
 
+// connect db
+connectDB();
+
+// Route files
+const bootcamps = require('./routes/bootcamps')
+
 const app = express()
+
+// body parser
+app.use(express.json())
 
 // dev logging middleware
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'))
@@ -20,6 +28,13 @@ app.use('/api/v1/bootcamps', bootcamps)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+const server = app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold);
 });
+
+// handle unhandled promised rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red)
+  // close server & exit process
+  server.close(() => process.exit(1))
+})
