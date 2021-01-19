03c79994682a98e7aecc64021d7b77a964a28add
Mohamad Handy Nugraha <handynugraha29@gmail.com>
2021-01-19 21:22:48 +0700

    add middleware geocoder

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const slugify = require('slugify');
+const geocoder = require('../utils/geocoder')
 
 const BootcampSchema = new mongoose.Schema({
   name: {
@@ -103,6 +104,24 @@ const BootcampSchema = new mongoose.Schema({
 // pakai function biasa supaya gak bingung karena masalah this di mongoose middleware
 BootcampSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true })
+  next()
+})
+
+// Geocode & create location field
+BootcampSchema.pre('save', async function(next) {
+  const loc = await geocoder.geocode(this.address)
+  this.location = {
+    type: 'Point',
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress,
+    street: loc[0].streetName,
+    city: loc[0].city,
+    zipcode: loc[0].zipCode,
+    state: loc[0].stateCode,
+    country: loc[0].countryCode
+  }
+  // do not save address in DB
+  this.address = undefined
   next()
 })
 
