2df099fab763a22df6f92ba325d5661747292ad2
Mohamad Handy Nugraha <handynugraha29@gmail.com>
2021-01-23 22:05:12 +0700

    add seeder for devcamper

---
+++ seeder.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const colors = require('colors');
+const dotenv = require('dotenv');
+
+// load env files
+dotenv.config( { path: './config/config.env' } )
+
+// load models
+const Bootcamp = require('./models/Bootcamp')
+
+// connect to DB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+})
+
+// read JSON file
+const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8'))
+
+
+// import into DB
+const importData = async () => {
+  try {
+    await Bootcamp.create(bootcamps)
+    console.log('data imported...'.green.inverse)
+    process.exit()
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+// delete data
+const deleteData = async () => {
+  try {
+    await Bootcamp.deleteMany()
+    console.log('data destroyed...'.red.inverse)
+    process.exit()
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+if (process.argv[2] === '-i') {
+  importData()
+} else if (process.argv[2] === '-d') {
+  deleteData()
+}
