5cfeeda4db50c04e9c8c2e6726ec1c98e231f7eb
Mohamad Handy Nugraha <handynugraha29@gmail.com>
2021-01-27 07:02:05 +0700

    video 48 create post user login api

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,7 +3,7 @@ const asyncHandler = require('../middleware/async')
 const User = require('../models/User')
 
 // @desc Register user
-// @routes GET /api/v1/auth/register
+// @routes POST /api/v1/auth/register
 // @access public
 exports.register = asyncHandler(async(req, res, next) => {
   const { name, email, password, role } = req.body
@@ -15,6 +15,38 @@ exports.register = asyncHandler(async(req, res, next) => {
     password,
     role
   })
+
+  // create token, user huruf kecil karena methods
+  const token = user.getSignedJwtToken()
+
+  res.status(200).json({ success: true, token })
+
+})
+
+// @desc Login user
+// @routes POST /api/v1/auth/login
+// @access public
+exports.login = asyncHandler(async(req, res, next) => {
+  const { email, password } = req.body
+
+  //validate email and password
+  if (!email || !password) {
+    return next(new ErrorResponse('Please provide an email and password', 400))
+  }
+
+  // check for user
+  const user = await User.findOne({ email }).select('+password')
+
+  if (!user) {
+    return next(new ErrorResponse('Invalid credentials', 401))
+  }
+
+  // check if password matches
+  const isMatch = await user.matchPassword(password)
+
+  if (!isMatch) {
+    return next(new ErrorResponse('Invalid credentials', 401))
+  }
 
   // create token, user huruf kecil karena methods
   const token = user.getSignedJwtToken()
