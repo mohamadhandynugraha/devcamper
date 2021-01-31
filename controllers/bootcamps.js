c0a152776900624cdb05b1b33f871f0b257d7f2a
Mohamad Handy Nugraha <handynugraha29@gmail.com>
2021-01-31 19:19:34 +0700

    video 53 Bootcamp and user relationship

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -26,6 +26,15 @@ exports.getBootcamp = asyncHandler(async (req, res, next) => {
 // @routes POST /api/v1/bootcamps/
 // @access private
 exports.createBootcamp = asyncHandler(async (req, res, next) => {
+  // add user to req.body
+  req.body.user = req.user.id
+  // check for publish bootcamp
+  const publishedBootcamp = await Bootcamp.findOne({ user: req.user.id })
+  // if the user is not an admin, they can only add one bootcamp
+  if (publishedBootcamp && req.user.role !== 'admin') {
+    return next(new ErrorResponse(`The user with ID ${req.user.id} has already published bootcamp`, 400))
+  }
+
   const bootcamp = await Bootcamp.create(req.body)
   res.status(201).json({
     success: true,
--- seeder.js
+++ seeder.js
@@ -9,6 +9,7 @@ dotenv.config( { path: './config/config.env' } )
 // load models
 const Bootcamp = require('./models/Bootcamp')
 const Course = require('./models/Course')
+const User = require('./models/User')
 
 // connect to DB
 mongoose.connect(process.env.MONGO_URI, {
@@ -23,12 +24,15 @@ const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`
 
 const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8'))
 
+const users = JSON.parse(fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8'))
+
 
 // import into DB
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps)
     await Course.create(courses)
+    await User.create(users)
     console.log('data imported...'.green.inverse)
     process.exit()
   } catch (error) {
@@ -41,6 +45,7 @@ const deleteData = async () => {
   try {
     await Bootcamp.deleteMany()
     await Course.deleteMany()
+    await User.deleteMany()
     console.log('data destroyed...'.red.inverse)
     process.exit()
   } catch (error) {
